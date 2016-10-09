d7da9b7e4591da152ecbd08ffba56e9721c72830
jeevanjayaprakash <jeevan55@gmail.com>
2016-10-09 19:15:28 +0100

    initial commit

---
+++ eloquent_javascript.md
@@ -0,0 +1,3 @@
+# Eloquent JavaScript
+
+Contains my solutions to the problems from Eloquent JavaScript. 
---
+++ looping-a-triangle.js
@@ -0,0 +1,14 @@
+/**
+ * Looping a triangle
+ *
+ * Jeevan Jayaprakash
+ * jeevan.jayaprakash@cantab.net
+ *
+ * Write a loop that outputs a triangle made up of hashes of height 7. 
+ */
+
+ var output = "";
+
+for(var i = 0; i < 7; i++) {
+  console.log(output+="#");
+}
